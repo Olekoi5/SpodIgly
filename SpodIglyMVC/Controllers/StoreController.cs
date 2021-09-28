dddf432757fb4132f30bbf044f078829f9029c6d
Gondocha234@outlook.com <Gondocha234@outlook.com>
2021-09-28 19:27:54 +0200

    Dodanie widoków do podstron i StroreController

---
+++ SpodIglyMVC/Controllers/StoreController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpodIglyMVC.Controllers
+{
+    public class StoreController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Kontakt()
+        {
+            return View();
+        }
+
+        public IActionResult Onas()
+        {
+            return View();
+        }
+
+        public IActionResult Regulamin()
+        {
+            return View();
+        }
+
+        public IActionResult Wysylka()
+        {
+            return View();
+        }
+    }
+}
