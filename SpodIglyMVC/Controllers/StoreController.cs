83b36f4dd837dab8c478d299ca30918ff4905b66
Gondocha234@outlook.com <Gondocha234@outlook.com>
2021-10-04 22:07:12 +0200

    Dodanie modeli i DbContext

--- SpodIglyMVC/Controllers/StoreController.cs
+++ SpodIglyMVC/Controllers/StoreController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SpodIglyMVC.DAL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
---
+++ SpodIglyMVC/Models/Album.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpodIglyMVC.Models
+{
+    public class Album
+    {
+        public int AlbumId { get; set; }
+        public int GenreId { get; set; }
+        public string AlbumTitle { get; set; }
+        public string ArtistName { get; set; }
+        public DateTime DateAdd { get; set; }
+        public string CovertFileName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsBestseller { get; set; }
+        public bool IsMidden { get; set; }
+
+        public virtual Genre Genre { get; set; }
+    }
+}
